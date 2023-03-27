bf72b48d2468e1c13e6b19b68e7f493d80ded350
BogartDME <edwin.pietrowski@gmail.com>
2023-03-27 15:03:59 -0400

    setting up resolver.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,3 +2,46 @@ const { AuthenticationError } = require('apollo-server-express');
 //const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+
+const resolvers = {
+    Query: {
+        profiles: async () => {
+            return Profile.find();
+        },
+    
+        profile: async (parent, { profileId }) => {
+            return Profile.findOne({ _id: profileId });
+        },
+        // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+        me: async (parent, args, context) => {
+            if (context.user) {
+            return Profile.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+
+    Mutation: {
+        addProfile: async (parent, { name, email, password }) => {
+          const profile = await Profile.create({ name, email, password });
+          const token = signToken(profile);
+    
+          return { token, profile };
+        },
+        login: async (parent, { email, password }) => {
+          const profile = await Profile.findOne({ email });
+    
+          if (!profile) {
+            throw new AuthenticationError('No profile with this email found!');
+          }
+    
+          const correctPw = await profile.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect password!');
+          }
+    
+          const token = signToken(profile);
+          return { token, profile };
+        },
