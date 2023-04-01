56b31a586f8ac2343e99dcedf95a7222a9cc08c1
difurung <dfurung@hotmail.com>
2023-03-31 21:48:18 -0400

    Removed fluid from fluid style

--- client/src/components/MainContainer.jsx
+++ client/src/components/MainContainer.jsx
@@ -3,10 +3,8 @@ import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import NavBar from "./NavBar";
 import ContactUs from "./pages/ContactUs";
-import Provider from "./pages/Provider";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import { Container } from "react-bootstrap";
 
 const MainContainer = () => {
   const [currentPage, setCurrentPage] = useState("Home");
@@ -21,9 +19,6 @@ const MainContainer = () => {
     if (currentPage === "Login") {
       return <Login />;
     }
-    if (currentPage === "Provider") {
-      return <Provider />;
-    }
     if (currentPage === "Dashboard") {
       return <Dashboard />;
     }
@@ -32,7 +27,7 @@ const MainContainer = () => {
   const handlePageChange = (page) => setCurrentPage(page);
   return (
     <div
-      fluid
+      
       style={{
         background: "linear-gradient(to right, rgb(14,39,67), #141c25)",
         // backgroundImage: `url("https://wallpaperaccess.com/full/4178252.jpg")`,
@@ -46,9 +41,9 @@ const MainContainer = () => {
     >
       {/* We are passing the currentPage from state and the function to update it */}
       <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
-    {/* Here we are calling the renderPage method which will return a component  */}
+      {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
-      </div>
-    );
-  };
+    </div>
+  );
+};
 export default MainContainer;
