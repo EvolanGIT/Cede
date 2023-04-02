12174f3d459d3f66f3ed85894a3d23465666be3d
Seabass <beattycharles@yahoo.com>
2023-04-02 17:10:38 -0400

    adjusting flow or dashboard

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -13,11 +13,11 @@ import AuthService from "../../utils/auth";
 import { RETURN_ONE_CUSTOMER } from "../../utils/queries";
 
 function Dashboard(props) {
-  //  Grab id from localstorage
-  // localStorage.getItem(id);
   //grab query to find One customer and pass id using useQuery hook
-  //destructure loading and data from the hook
   const { data, loading, error } = useQuery(RETURN_ONE_CUSTOMER);
+  //  Grab id from localstorage
+  localStorage.getItem(_id);
+  //destructure loading and data from the hook
 
   const token = AuthService.loggedIn() ? AuthService.getToken() : null;
   if (!token) {
@@ -27,68 +27,42 @@ function Dashboard(props) {
   console.log(data);
   //create ternary where loading if true shows loading screen else show data.
 
-  const loggedIn = () => {
-    if (loading) return "Loading...";
-    if (error) return <h1 style={{ color: "Red" }}>{error.message}</h1>;
-    const customer = data?.customers;
+  // const loggedIn = () => {
+  if (loading) return "Loading...";
+  if (error) return <h1 style={{ color: "Red" }}>{error.message}</h1>;
+  const customer = data?.customers;
 
-    // return <Link to="/Dashboard" />;
-    // {
-    //   firstName: "Ben",
-    //   lastName: "Salem",
-    //   dob: "01/01/1980",
-    //   email: "ben@mail.com",
-    //   gender: "Male",
-    //   phoneNumber: "123-456-7890",
-    //   bloodType: "AB+",
-    //   doNotResuscitate: false,
-    //   doNotIntubate: true,
-    //   allergies: ["Peanuts", "Penicillin"],
-    //   emergencyContacts: [
-    //     {
-    //       firstName: "Some",
-    //       lastName: "Body",
-    //       phoneNumber: "234-567-8901",
-    //     },
-    //     {
-    //       firstName: "Bob",
-    //       lastName: "Salem",
-    //       phoneNumber: "345-678-9012",
-    //     },
-    //   ],
-    // };
+  // This renders the patient information page
+  return (
+    <Container>
+      <Card
+        className="shadow-lg"
+        style={{
+          alignContent: "center",
+          backgroundColor: "#0e385c",
+          color: "#959696",
+        }}
+      >
+        <div style={{ backgroundColor: "#0e385c", color: "#959696" }}>
+          <h1>Customer Information Sheet</h1>
+          <div>
+            <h2>Personal Information</h2>
 
-    // This renders the patient information page
-    return (
-      <Container>
-        <Card
-          className="shadow-lg"
-          style={{
-            alignContent: "center",
-            backgroundColor: "#0e385c",
-            color: "#959696",
-          }}
-        >
-          <div style={{ backgroundColor: "#0e385c", color: "#959696" }}>
-            <h1>Customer Information Sheet</h1>
-            <div>
-              <h2>Personal Information</h2>
+            <p>First Name: {customer.firstName}</p>
+            <p>Last Name: {customer.lastName}</p>
+            <p>DoB: {customer.birthdate}</p>
+            <p>Email: {customer.email}</p>
+            <p>Gender: {customer.gender}</p>
+            <p>Phone Number: {customer.phoneNumber}</p>
 
-              <p>First Name: {customer.firstName}</p>
-              <p>Last Name: {customer.lastName}</p>
-              <p>DoB: {customer.birthdate}</p>
-              <p>Email: {customer.email}</p>
-              <p>Gender: {customer.gender}</p>
-              <p>Phone Number: {customer.phoneNumber}</p>
+            <h2>Clinical Information</h2>
+            <p>Blood Type: {customer.bloodType}</p>
+            <p>
+              Do not Resuscitate: {customer.doNotResuscitate ? "Yes" : "No"}
+            </p>
+            <p>Do not Intubate: {customer.doNotIntubate ? "Yes" : "No"}</p>
 
-              <h2>Clinical Information</h2>
-              <p>Blood Type: {customer.bloodType}</p>
-              <p>
-                Do not Resuscitate: {customer.doNotResuscitate ? "Yes" : "No"}
-              </p>
-              <p>Do not Intubate: {customer.doNotIntubate ? "Yes" : "No"}</p>
-
-              {/* <h3>Allergies</h3>
+            {/* <h3>Allergies</h3>
             <ul class="nopadding" style={{ listStyle: "none" }}>
               {customer.allergies.map((allergy, index) => (
                 <li key={index}>{allergy}</li>
@@ -104,13 +78,39 @@ function Dashboard(props) {
                     Phone Number: {contact.phoneNumber}
                   </div>
                 </li> */}
-              {/* ))}
+            {/* ))}
             </ul> */}
-            </div>
           </div>
-        </Card>
-      </Container>
-    );
-  };
+        </div>
+      </Card>
+    </Container>
+  );
 }
+
 export default Dashboard;
+
+// return <Link to="/Dashboard" />;
+// {
+//   firstName: "Ben",
+//   lastName: "Salem",
+//   dob: "01/01/1980",
+//   email: "ben@mail.com",
+//   gender: "Male",
+//   phoneNumber: "123-456-7890",
+//   bloodType: "AB+",
+//   doNotResuscitate: false,
+//   doNotIntubate: true,
+//   allergies: ["Peanuts", "Penicillin"],
+//   emergencyContacts: [
+//     {
+//       firstName: "Some",
+//       lastName: "Body",
+//       phoneNumber: "234-567-8901",
+//     },
+//     {
+//       firstName: "Bob",
+//       lastName: "Salem",
+//       phoneNumber: "345-678-9012",
+//     },
+//   ],
+// };
