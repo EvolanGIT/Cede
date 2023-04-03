12f81923dbdbddb61eda0bf5c29ca5b88c35d967
beattycharles <113803621+beattycharles@users.noreply.github.com>
2023-04-03 00:06:33 -0400

    remove dup code and removing id from storage.

--- client/src/components/pages/SignUp.jsx
+++ client/src/components/pages/SignUp.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,45 +1,46 @@
-    import decode from 'jwt-decode';
+import decode from "jwt-decode";
 
-    class AuthService {
-    getProfile() {
-        return decode(this.getToken());
+class AuthService {
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
     }
+  }
 
-    loggedIn() {
-        // Checks if there is a saved token and it's still valid
-        const token = this.getToken();
-        return !!token && !this.isTokenExpired(token);
-    }
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem("id_token");
+  }
 
-    isTokenExpired(token) {
-        try {
-        const decoded = decode(token);
-        if (decoded.exp < Date.now() / 1000) {
-            return true;
-        } else return false;
-        } catch (err) {
-        return false;
-        }
-    }
+  login(idToken) {
+    // Saves user token to localStorage
+    localStorage.setItem("id_token", idToken);
 
-    getToken() {
-        // Retrieves the user token from localStorage
-        return localStorage.getItem('id_token');
-    }
+    window.location.assign("/dashboard");
+  }
 
-    login(idToken) {
-        // Saves user token to localStorage
-        localStorage.setItem('id_token', idToken);
-
-        window.location.assign('/dashboard');
-    }
-
-    logout() {
-        // Clear user token and profile data from localStorage
-        localStorage.removeItem('id_token');
-        // this will reload the page and reset the state of the application
-        window.location.assign('/');
-    }
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("_id");
+    // this will reload the page and reset the state of the application
+    window.location.assign("/");
+  }
 }
 
-export default new AuthService;
+export default new AuthService();
