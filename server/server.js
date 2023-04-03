0138b945117e00f3496342c37e2475b465bbd357
YonB <yon_baermund@hotmail.com>
2023-04-03 18:27:13 -0400

    debug on the router for deployment

--- server/server.js
+++ server/server.js
@@ -17,8 +17,8 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
+// // Serve up static assets
+// app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
